0a3efbc6de4316e628abe85ed80d149a754110f9
Ravaren <radoslawpanek@epoczta.pl>
2018-12-15 17:59:43 +0100

    Dodałem kilka starych aplikacji

--- src/zadaniajkozak/ZadaniaJKozak.java
+++ src/zadaniajkozak/ZadaniaJKozak.java
@@ -15,13 +15,24 @@ public class ZadaniaJKozak {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("41. „Abrakadabra” - ile razy ostatnia litera występuje.");
+        System.out.println("42. Kot - tok. Wyrazy od tyłu.");
+        System.out.println();
         System.out.println("51. Silnia. Iteracyjnie i rekurencyjnie.");
         System.out.println("52. Ciag Fibonacciego. Iteracyjnie i rekurencyjnie.");
         System.out.print("Wybierz ćwiczenie do uruchomienia: ");
+        System.out.println();
         Scanner odczyt = new Scanner(System.in);
         int wybor = odczyt.nextInt();
         switch(wybor){
             
+            case 41:    Zaj4zad1 zadanie41 = new Zaj4zad1(); 
+                        zadanie41.zadanie();
+                        break;
+            case 42:    Zaj4zad2 zadanie42 = new Zaj4zad2(); 
+                        zadanie42.zadanie();
+                        break;           
+                        
             case 51:    Zaj5zad1 zadanie51 = new Zaj5zad1(); 
                         zadanie51.zadanie();
                         break;
