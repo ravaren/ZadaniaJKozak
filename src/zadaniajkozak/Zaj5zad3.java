bab205fcae803066b5284081aee5cc8baaedf22f
Ravaren <radoslawpanek@epoczta.pl>
2018-12-15 18:26:02 +0100

    Dodałem klasę zadania 5.3

---
+++ src/zadaniajkozak/Zaj5zad3.java
@@ -0,0 +1,15 @@
+/**
+3. Zdefiniować funkcję int strpos(String text, char z) , która zwraca indeks
+na którym znajduje się znak z (drugi parametr) w podanym łańcuchu text .
+Jeżeli znak z nie występuje w łańcuchu, to wynikiem funkcji powinno być -1.
+Uwaga - pozycje znaków numerujemy począwszy od 0.
+*/
+package zadaniajkozak;
+
+public class Zaj5zad3{
+    
+    public static void zadanie(){
+        
+        
+    }
+}
