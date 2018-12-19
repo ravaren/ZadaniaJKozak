bd5054b5e9735c9ea5dbef8ead199c6f7ac2b3f7
ravaren <radoslawpanek@epoczta.pl>
2018-12-19 12:53:12 +0100

    Zrobilem zadanie 5.11

--- src/zadaniajkozak/ZadaniaJKozak.java
+++ src/zadaniajkozak/ZadaniaJKozak.java
@@ -34,7 +34,7 @@ public class ZadaniaJKozak {
         System.out.println();
         Scanner odczyt = new Scanner(System.in);
         //int wybor = odczyt.nextInt();
-        int wybor = 510;
+        int wybor = 511;
         switch(wybor){
             
             case 41:    Zaj4zad1 zadanie41 = new Zaj4zad1(); 
--- src/zadaniajkozak/Zaj5zad11.java
+++ src/zadaniajkozak/Zaj5zad11.java
@@ -14,7 +14,7 @@ package zadaniajkozak;
 public class Zaj5zad11 {
     String strcut(String str, int start, int ile){
         String newString = "";
-        
+        newString = str.substring(0,start) + str.substring(start+ile,str.length());
         
         return newString;
     }
@@ -23,8 +23,8 @@ public class Zaj5zad11 {
         System.out.println("511. Procedura strcut - wycina z łańcucha okreslony fragment.");
         System.out.println();
         
-        System.out.println("strcut(\"Ala ma kota\", 4, 3) - wynik to \"Ala kota\" = " + strcut("Ala ma kota", 4, 3));
-        System.out.println("strcut(\"Ala ma kota\", 0, 4) - wynik to \"ma kota\" = " + strcut("Ala ma kota", 0, 4));
-        System.out.println("strcut(\"Ala ma kota\", 0, 11) - wynik to \"\" = " + strcut("Ala ma kota", 0, 11));
+        System.out.println("strcut(\"Ala ma kota\", 4, 3) - wynik to \"Ala kota\" = " + "\"" + strcut("Ala ma kota", 4, 3) + "\"");
+        System.out.println("strcut(\"Ala ma kota\", 0, 4) - wynik to \"ma kota\" = " + "\"" + strcut("Ala ma kota", 0, 4) + "\"");
+        System.out.println("strcut(\"Ala ma kota\", 0, 11) - wynik to \"\" = " + "\"" + strcut("Ala ma kota", 0, 11) + "\"");
     }
 }
